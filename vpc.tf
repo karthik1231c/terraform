6d2ebc3d39be0cd02edc7fc443f48e540893b05f
Karthik <68536919+karthik1231c@users.noreply.github.com>
2021-05-13 03:16:45 +0000

    with keys

--- vpc.tf
+++ vpc.tf
@@ -1,3 +1,9 @@
+provider "aws" {
+  region = "us-east-1"
+  access_key = "AKIAUGFB7DFINFIQTR7E"
+  secret_key = "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"
+}
+
 resource "aws_vpc" "karthik" {
   cidr_block = "10.0.0.0/16"
 }
